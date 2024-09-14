aca531ed25d0a364d7999a1a02db95c7f1069fa5
Jimmy-Ou-UI <jimmy.ou@ui.com>
2024-09-14 17:00:29 +0800

    mcumgr-smp: support Setting Management group

---
+++ mcumgr-smp/src/setting_management.rs
@@ -0,0 +1,67 @@
+// Author: Sascha Zenglein <zenglein@gessler.de>
+// Copyright (c) 2023 Gessler GmbH.
+use crate::{Group, SmpFrame};
+
+use crate::OpCode::{ReadRequest, WriteRequest};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ReadSettingRequest {
+    pub name: String,
+}
+
+pub fn read_setting(sequence: u8, name: String) -> SmpFrame<ReadSettingRequest> {
+    let payload = ReadSettingRequest { name };
+
+    SmpFrame::new(ReadRequest, sequence, Group::SettingManagement, 0, payload)
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(untagged)]
+pub enum ReadSettingResult {
+    Ok {
+        #[serde(with = "serde_bytes")]
+        val: Vec<u8>,
+    },
+    Err {
+        rc: i32,
+    },
+}
+
+impl ReadSettingResult {
+    pub fn into_result(self) -> Result<Vec<u8>, i32> {
+        match self {
+            ReadSettingResult::Ok { val } => Ok(val),
+            ReadSettingResult::Err { rc } => Err(rc),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct WriteSettingRequest {
+    pub name: String,
+    #[serde(with = "serde_bytes")]
+    pub val: Vec<u8>,
+}
+
+pub fn write_setting(sequence: u8, name: String, val: Vec<u8>) -> SmpFrame<WriteSettingRequest> {
+    let payload = WriteSettingRequest { name, val };
+
+    SmpFrame::new(WriteRequest, sequence, Group::SettingManagement, 0, payload)
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(untagged)]
+pub enum WriteSettingResult {
+    Ok {},
+    Err { rc: i32 },
+}
+
+impl WriteSettingResult {
+    pub fn into_result(self) -> Result<(), i32> {
+        match self {
+            WriteSettingResult::Ok {} => Ok(()),
+            WriteSettingResult::Err { rc } => Err(rc),
+        }
+    }
+}
